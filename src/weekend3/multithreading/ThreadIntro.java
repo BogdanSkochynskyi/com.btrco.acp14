69601ccca7f7454a1aa9e4df260e7c6bbb2ccf2e
bogdanskochynskyi <bogdanskochynskyi@gmail.com>
2016-09-20 21:41:12 +0300

    intro working with threads

--- src/weekend3/multithreading/ThreadIntro.java
+++ src/weekend3/multithreading/ThreadIntro.java
@@ -2,9 +2,32 @@ package weekend3.multithreading;
 
 public class ThreadIntro {
 
+    public static void main(String[] args) {
+        //get current thread
+        Thread main = Thread.currentThread();
+        //Thread.sleep(1000); // == Thread.currentThread.sleep(1000);
 
+        System.out.println(main.getName());
+
+        MyThread myThread = new MyThread("simple thread");
+//Daemon поток - слабый поток, который зависит от мейна
+//Не демон поток - основная программа умерла, а ты можешь продолжать жить. Демон - умираешь вместе с главным потоком
+        myThread.setDaemon(true);
+        myThread.start();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("main working %d\n", i);
+            try {
+                main.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
+
+
 
 class MyThread extends Thread
 {
@@ -18,7 +41,8 @@ class MyThread extends Thread
     public void run() {
 
         while (!isInterrupted()) // пока не отправлен сигнал что этот поток не нужен - работает
-        { //как только кто то вы
+        { //как только кто то вызовет на этом потоке interrupt метод - вызовется эксепшн и в кэче можно довыполнить что то или просто продолжить свою работу.
+            //и чтобы окончательно закрыть поток - надо самому на себе вызвать метод interrupt
             System.out.printf("My work %s, %s, %s\n", getId(), getName(), getState());
             try {
                 sleep(1000); //не доступиться к ресурсам пока спишь
